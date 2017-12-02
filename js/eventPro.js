391ebc00b5b047ddfc131c3d223abb63713500f5
Ivy Lin <littlebugivy@gmail.com>
2017-12-01 20:51:59 -0500

    js added

--- js/eventPro.js
+++ js/eventPro.js
@@ -1,17 +1,46 @@
 $('.carousel').carousel({
     interval: 2000
 })
+
+var counter = 0;
+var object = [
+    {"event_img" : "img",
+    "event_name":"event1"},
+    {"event_img" : "img",
+    "event_name":"event1"},
+    {"event_img" : "img",
+    "event_name":"event1"},
+    {"event_img" : "img",
+    "event_name":"event1"},
+    {"event_img" : "img",
+    "event_name":"event1"},
+    {"event_img" : "img",
+    "event_name":"event1"}
+]
+
 $(function () {
     $(".heart").on("click", function () {
         $(this).toggleClass("is-active");
     });
 });
 
-$(function(){
-    $(".seemore").on("click",function(){
-        console.log('see more')
-        $(".search_result").append("<div class='search_result'><div class='col-lg-3'><div class='event_card'><a href='{{result_link}}' class='postcard_body'><div class='postcard_image'><img src='#' alt='result_img' class='search_result_img'></div> <div class='postcard_content'><div class='event_title'>{{EventName}}</div><div class='event_date_location'>{{EventDate}}<br>{{Event_Location}}</div></div></a><div class='row card-bottom-container'><div class='event_price col-lg-8'>{{EventPrice}}</div><div class='heart col-lg-4'></div></div></div></div>")
-    })
+$(function(){  
+    console.log('see more')
+
+    var length = object.length;
+
+    console.log('length: ', length);
+    console.log('counter: ', counter);
+
+    if (length>4){
+        var i=0;
+        while(i<4){
+            $(".seemore").on("click",function(){  
+                $(".search_result").append("<div class='col-lg-3'><div class='event_card'><a href='{{result_link}}' class='postcard_body'><div class='postcard_image'><img src='#' alt='result_img' class='search_result_img'></div> <div class='postcard_content'><div class='event_title'>{{EventName}}</div><div class='event_date_location'>{{EventDate}}<br>{{Event_Location}}</div></div></a><div class='row card-bottom-container'><div class='event_price col-lg-8'>{{EventPrice}}</div><div class='heart col-lg-4'></div></div></div>")
+            });
+            i++;
+        }
+    }
 })
 
 $(function(){
