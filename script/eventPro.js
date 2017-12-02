dc1d985e13611f08f96962421b76f4b8e4243708
Ivy Lin <littlebugivy@gmail.com>
2017-12-02 15:54:36 -0500

    heart - debug

--- script/eventPro.js
+++ script/eventPro.js
@@ -17,7 +17,7 @@ $(function(){
         $('.search-form').css("bottom", newheight*0.2)
         $('.event_postcard').height($('.event_postcard').width())
         var card_height = $('.event_postcard').height();
-        var ratio = -1*card_height/240*90;
+        var ratio = -1*card_height/240*100;
         var heart_height= card_height*90/240;
         $('.fa-heart').height(heart_height+'px');
         $('.fa-heart').width(heart_height+'px');
@@ -59,7 +59,7 @@ $(function(){
         var i=0;
         while(i<4){
             $(".seemore").on("click",function(){  
-                $(".search_result").append("<div class='col-lg-3'><div class='event_card'><a href='{{result_link}}' class='postcard_body'><div class='postcard_image'><img src='#' alt='result_img' class='search_result_img'></div> <div class='postcard_content'><div class='event_title'>{{EventName}}</div><div class='event_date_location'>{{EventDate}}<br>{{Event_Location}}</div></div></a><div class='row card-bottom-container'><div class='event_price col-lg-8'>{{EventPrice}}</div><div class='heart col-lg-4'></div></div></div>")
+                $(".item-list").append("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'><div id='item-G5vYZfSfPYh_1' class='event_postcard item' data-item_id='G5vYZfSfPYh_1' data-favorite='undefined'><div class='postcard_image'><img src='https://s1.ticketm.net/dam/a/375/606d24be-8ead-40aa-a622-22e5dfd02375_562662_RETINA_LANDSCAPE_16_9.jpg' class='search_result_img' alt='item image'></div><div class='postcard_content'><div class='event_title'><a href='http://www.ticketmaster.com/ozuna-odiesa-society-tour-san-jose-california-12-03-2017/event/1C00533525C1B64A' target='_blank' class='item-name'>Ozuna Odiesa Society Tour</a></div><div class='event_date_location'><p class='item-address'>525 W Santa Clara<br>San Jose</p></div></div><div class='event_price col-8'><p class='item-category'>Music</p></div><div class='event_heart fav-link col-4 fa-heart'></div></div></div>")
             });
             i++;
         }
