29ed5c177f62dbb021663430447e173f6ee5114c
Ivy Lin <littlebugivy@gmail.com>
2017-12-04 09:15:06 -0500

    profile edit function

--- script/eventPro.js
+++ script/eventPro.js
@@ -207,3 +207,45 @@ $(function(){
     })   
 })
 
+
+$(function() {
+        var readURL = function(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+    
+                reader.onload = function (e) {
+                    $('.profile-pic').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        
+        $(".file-upload").on('change', function(){
+            readURL(this);
+        });
+        
+        $(".upload-button").on('click', function() {
+           $(".file-upload").click();
+        });
+    });
+
+
+    $(function(){
+        $('.editorsave').on('click', function(){
+            if($(this).html() =='Edit'){
+                console.log('edit')
+                $(this).html('Save');
+                $('.file-upload').show()
+                $(".form-control").attr("readonly", false);                 
+                $(".alert").hide();
+            }else{
+                console.log('save')
+                $(this).html('Edit');
+                $(".alert").show();
+       
+                $('.file-upload').hide()
+                $(".form-control").attr("readonly", true); 
+               
+            }  
+        })
+    })
